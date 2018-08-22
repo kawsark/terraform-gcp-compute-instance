e0f9e66b49ec34a8574dba638d6389a5559958d4
kawsark <kawsar@hashicorp.com>
2018-08-22 13:13:23 -0400

    simplified startup script

--- startup-script.sh
+++ startup-script.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-cd /tmp
-sudo apt-get update -y
-sudo apt-get install wget git curl jq -y
-git clone https://github.com/hashicorp/guides-configuration.git
 echo "This user-data was input by Terrafom by $(whoami) @ $(date)" > /tmp/startup-script.txt
