ae251b1fe40e33c4bc19d059029ec9116969583e
kawsark <kawsar@hashicorp.com>
2019-04-29 14:53:02 -0400

    update output to include splat operator

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "external_ip"{
-  value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "${google_compute_instance.demo.*.network_interface.0.access_config.0.assigned_nat_ip}"
 }
 
