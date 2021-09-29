cc42eebc53281276ccc1f03fd1fccd8332ca175d
kawsark <kawsar@hashicorp.com>
2021-09-29 17:30:52 -0400

    removed apply.sh

--- examples/vault-ubuntu-server-ha/apply.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-terraform0.11.14 apply -auto-approve
-sleep 10
-terraform0.11.14 apply -auto-approve
-sleep 10
-gcloud beta compute --project "kawsar-kamal-gcp2" ssh --zone "us-east1-b" "vault0-0"
